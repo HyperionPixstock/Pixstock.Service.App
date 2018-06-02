2e4429af6fd450740bdbe25c2752740c1d685dc5
atachimiko <atachi.miko@gmail.com>
2018-06-02 11:43:02 +0900

    Init

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+
+namespace Pixstock.Service.App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseNLog() // NLog: setup NLog for Dependency injection
+                .UseUrls("http://localhost:5080")
+                .Build();
+    }
+}
